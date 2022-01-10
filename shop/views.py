8609c37bab8abc98c8eb3749bf65ac3ac0627f11
paullally <paullally98@gmail.com>
2022-01-10 22:27:21 +0000

    add shop page

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def all_products(request):
+    """show all products """
+    products = Product.objects.all()
+    
+
+    context = {
+        'products':products,
+
+    }
+
+    
+    return render(request,'shop/shop.html', context)
