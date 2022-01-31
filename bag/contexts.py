f9dbf27d43ffd056839c5eb9a0acc660d2263245
paullally <paullally98@gmail.com>
2022-01-31 19:50:44 +0000

    update add to bag functions

---
+++ bag/contexts.py
@@ -0,0 +1,33 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from shop.models import Product
+
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    grand_total = total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'grand_total': grand_total,
+    }
+    
+    return context
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse, HttpResponse
 def view_bag(request):
     """ renders bag """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print("ASDASDASDASDAS")
+    print(request.session['bag'])
+    return redirect(view_bag)
