4f359451e3cb53b1ea73a05078b3a444f238952c
paullally <paullally98@gmail.com>
2022-02-02 21:15:11 +0000

    set up donation ammount model

--- monthly_donation/models.py
+++ monthly_donation/models.py
@@ -1,3 +1,13 @@
+from django.db.models.signals import post_save
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+DONATION_AMOUNT = (
+    ('0', '0'),
+    ('10', '10'),
+    ('20', '20'),
+    ('50', '50'),
+)
+
+
