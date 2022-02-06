c36b328e2caa74e71cd2d0112ce7c71b75630513
paullally <paullally98@gmail.com>
2022-02-06 19:20:03 +0000

    fix issue with stripe

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,7 +7,7 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
-                  'town_or_city', 'postcode', 'county',)
+                  'town_or_city', 'postcode', 'country',)
 
     def __init__(self, *args, **kwargs):
         """
@@ -23,7 +23,7 @@ class OrderForm(forms.ModelForm):
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'country': 'Country',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
