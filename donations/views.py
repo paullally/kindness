161f5b97f446ee8daa105c933173376024377736
paullally <paullally98@gmail.com>
2022-02-07 19:59:43 +0000

    create get user membership function

--- donations/views.py
+++ donations/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.conf import settings
+from django.shortcuts import render, redirect, reverse
+from .models import Membership, UserMembership, Subscription
+from django.contrib.auth.decorators import login_required
+from .forms import SubscriptionForm
+from django.contrib import messages
+
+
+import stripe
+
+stripe_public_key = settings.STRIPE_PUBLIC_KEY
+stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+@login_required()
+def get_user_membership(request):
+    user_membership_qs = UserMembership.objects.filter(user=request.user)
+    if user_membership_qs.exists():
+        return user_membership_qs.first()
+    return None
+
