06f249f94410ce30ae272838e508d31e85177add
paullally <paullally98@gmail.com>
2022-02-07 12:57:57 +0000

    added models to admin.py

--- monthly_donation/admin.py
+++ monthly_donation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import  Donors,  Active, Donation
+
+admin.site.register(Donors)
+admin.site.register(Active)
+admin.site.register(Donation)
