d9bd2814dd7fd6223cb9b866db8aef3e14652451
paullally <paullally98@gmail.com>
2022-02-07 21:27:46 +0000

    added slected memebership function

--- donations/views.py
+++ donations/views.py
@@ -12,17 +12,27 @@ stripe_public_key = settings.STRIPE_PUBLIC_KEY
 stripe_secret_key = settings.STRIPE_SECRET_KEY
 
 @login_required()
-def get_user_membership(request):
-    user_membership_qs = UserMembership.objects.filter(user=request.user)
-    if user_membership_qs.exists():
-        return user_membership_qs.first()
+def get_membership(request):
+    membership = UserMembership.objects.filter(user=request.user)
+    if membership.exists():
+        return membership.first()
     return None
 
 @login_required()
-def get_user_subscription(request):
-    user_subscription_qs = Subscription.objects.filter(
-        user_membership=get_user_membership(request))
-    if user_subscription_qs.exists():
-        user_subscription = user_subscription_qs.first()
+def get_subscription(request):
+    subscription = Subscription.objects.filter(
+        membership=getmembership(request))
+    if subscription.exists():
+        user_subscription = subscription.first()
         return user_subscription
+    return None
+
+
+@login_required()
+def selected_membership(request):
+    membership_type = request.session['selected_membership_type']
+    membership = Membership.objects.filter(
+        membership_type=membership_type)
+    if membership.exists():
+        return membership.first()
     return None
