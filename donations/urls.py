9773c3d954fd5cd61abdb2da810650727087ed01
paullally <paullally98@gmail.com>
2022-02-08 16:26:14 +0000

    update donations page

--- donations/urls.py
+++ donations/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+   path('', views.overview, name='overview'),
    path('plan/', views.donations, name='donations'),
    path('checkout/', views.checkout, name='checkout'),
-    path('overview/', views.overview, name='overview'),
-
+   
    
 ]
