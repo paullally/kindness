56a173646be16021c6b75b55a12d7cceeecf7563
paullally <paullally98@gmail.com>
2022-02-09 16:54:19 +0000

    removed secret key

--- kindness/settings.py
+++ kindness/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q_us18_z)wjr$^h@hi2tp4n4@zg6$1m*a9a&b#ofsn-&0#*(sv'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
